3d1ddc8e9657f2e0d2f82a7f0a55be1b28979aee
Klatona <78326069+Klatona@users.noreply.github.com>
2021-05-17 21:32:23 -0400

    Added Search Input to Dictionary.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
 import './App.css';
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <div className="card body">
+        <div className="card-body background">
       <header className="App-header">
-    Dictionary
-      
+    <h1>
+      Dictionary
+      </h1>
       </header>
-   
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="Kristen"> 
+        Coded by Kristen
+      </footer>
+   </div>
+   </div>
     </div>
   );
 }
 
-export default App;
+
